f0595126c6674da5c430541f70b0813b1c07870a
Kevin Figueroa <kevin.figueroa@labo-ve.fr>
2020-10-08 20:15:58 +0200

    add delete methods

--- delete.php
+++ delete.php
@@ -1,3 +1,45 @@
 <?php
-    
+    function deletePost($id)
+	{
+		global $conn;
+		$query = "DELETE FROM post WHERE id=".$id;
+		if(mysqli_query($conn, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'status_message' =>'Post supprime avec succes.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'status_message' =>'La suppression du post a echoue. '. mysqli_error($conn)
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+    }
+
+    function deleteTopic($id)
+	{
+		global $conn;
+		$query = "DELETE FROM topic WHERE id=".$id;
+		if(mysqli_query($conn, $query))
+		{
+			$response=array(
+				'status' => 1,
+				'status_message' =>'Topic supprime avec succes.'
+			);
+		}
+		else
+		{
+			$response=array(
+				'status' => 0,
+				'status_message' =>'La suppression du topic a echoue. '. mysqli_error($conn)
+			);
+		}
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
 ?>
